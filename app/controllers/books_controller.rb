898eec4893ed86d7398ba4b14a07f6402fc110c0
CT1374996 <ec2-user@ip-172-31-29-162.ap-northeast-1.compute.internal>
2023-06-16 15:51:15 +0000

    [add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,14 +7,17 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
-  end
-
   def show
     @book = Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 
   def edit
@@ -25,6 +28,12 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
